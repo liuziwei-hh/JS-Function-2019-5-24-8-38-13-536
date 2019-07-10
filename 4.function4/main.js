630d3d030279402bdc2f31fbd9ed8cdb3a0ac309
liuziwei-hh <52273798+liuziwei-hh@users.noreply.github.com>
2019-07-10 10:29:42 +0800

    Create main.js

---
+++ 4.function4/main.js
@@ -0,0 +1,6 @@
+function countWords(message) { 
+ // wirte your code here 
+ var a=message.split(" "); 
+ console.log(a.length); 
+ } 
+ countWords('Good morning, I love JavaScript.'); // should return 5 
