f2017f0c9714386190353185580a629fc344814f
liuziwei-hh <52273798+liuziwei-hh@users.noreply.github.com>
2019-07-10 09:55:45 +0800

    Create main.js

---
+++ 1.function1/main.js
@@ -0,0 +1,6 @@
+function reverseString(message){ 
+ // wirte your code here 
+ var b=message.split(""); 
+ console.log(b.reverse().join('')); 
+ } 
+ reverseString('hello'); // should return 'olleh' 
